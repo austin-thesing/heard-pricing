2d7f62cfeb6bb54318fcf8c5b661a0bc444ad1d8
Austin Thesing <athesing@point.com>
2023-10-23 16:35:58 -0600

    Updated logic for specialized labels under price

--- pricing-swap.js
+++ pricing-swap.js
@@ -1,72 +1,77 @@
 // Pricing information
 const pricing = {
-    'Sole Proprietor': {
-      monthly: '199',
-      annually: '169'
-    },
-    'S Corporation': {
-      monthly: '299',
-      annually: '255'
-    },
-    'Sole Proprietor or S Corporation': {
-      monthly: '299',
-      annually: '255'
-    }
+  "Sole Proprietor": {
+    monthly: "199",
+    annually: "169",
+  },
+  "S Corporation": {
+    monthly: "299",
+    annually: "255",
+  },
+  "Sole Proprietor or S Corporation": {
+    monthly: "299",
+    annually: "255",
+  },
 };
 
 // Billing details
 const billingDetails = {
-  monthly: 'Billed monthly • Cancel anytime',
-  annually: 'Billed annually • Annual commitment'
+  monthly: "Billed monthly • Cancel anytime",
+  annually: {
+    "Sole Proprietor": "Billed $2,028 annually • Annual commitment",
+    "S Corporation": "Billed $3,060 annually • Annual commitment",
+    "Sole Proprietor or S Corporation":
+      "Billed $3,060 annually • Annual commitment",
+  },
 };
 
 // Function to update pricing
 function updatePricing(card) {
   // Get the card type
-  const cardTypeElement = card.querySelector('.n-pricing-plan-type div');
+  const cardTypeElement = card.querySelector(".n-pricing-plan-type div");
   if (!cardTypeElement) {
-    console.log('Card type element not found');
+    console.log("Card type element not found");
     return;
   }
   const cardType = cardTypeElement.textContent.trim();
 
   // Get the price per month element
-  const pricePerMonth = card.querySelector('.price-per-month');
+  const pricePerMonth = card.querySelector(".price-per-month");
   if (!pricePerMonth) {
-    console.log('Price per month element not found');
+    console.log("Price per month element not found");
     return;
   }
 
   // Get the billing cycle label element
-  const billingCycleLabel = card.querySelector('.billing-cycle-label');
+  const billingCycleLabel = card.querySelector(".billing-cycle-label");
   if (!billingCycleLabel) {
-    console.log('Billing cycle label element not found');
+    console.log("Billing cycle label element not found");
     return;
   }
 
   // Get the plan term details element
-  const planTermDetails = card.querySelector('.plan-term-details');
+  const planTermDetails = card.querySelector(".plan-term-details");
   if (!planTermDetails) {
-    console.log('Plan term details element not found');
+    console.log("Plan term details element not found");
     return;
   }
 
   // Update to annually
   pricePerMonth.textContent = pricing[cardType].annually;
-  billingCycleLabel.textContent = 'Annual Plan';
-  planTermDetails.textContent = billingDetails.annually;
+  billingCycleLabel.textContent = "Annual Plan";
+  planTermDetails.textContent = billingDetails.annually[cardType];
 }
 
 // Update pricing on page load
-document.querySelectorAll('.new-pricing-card').forEach(updatePricing);
+document.querySelectorAll(".new-pricing-card").forEach(updatePricing);
 
 // Select all toggles
-const toggles = document.querySelectorAll('.pricing-chart_toggle-container');
+const toggles = document.querySelectorAll(".pricing-chart_toggle-container");
 
 // Loop through each toggle
 toggles.forEach((toggle, index) => {
   // Add event listener for click event
-  toggle.addEventListener('click', function() {
+  toggle.addEventListener("click", function () {
     // Trigger click event on all other toggles
     toggles.forEach((otherToggle, otherIndex) => {
       if (otherIndex !== index) {
@@ -75,38 +80,38 @@ toggles.forEach((toggle, index) => {
     });
 
     // Get the parent card
-    const card = this.closest('.new-pricing-card');
+    const card = this.closest(".new-pricing-card");
     if (!card) {
-      console.log('Card not found');
+      console.log("Card not found");
       return;
     }
 
     // Get the card type
-    const cardTypeElement = card.querySelector('.n-pricing-plan-type div');
+    const cardTypeElement = card.querySelector(".n-pricing-plan-type div");
     if (!cardTypeElement) {
-      console.log('Card type element not found');
+      console.log("Card type element not found");
       return;
     }
     const cardType = cardTypeElement.textContent.trim();
 
     // Get the price per month element
-    const pricePerMonth = card.querySelector('.price-per-month');
+    const pricePerMonth = card.querySelector(".price-per-month");
     if (!pricePerMonth) {
-      console.log('Price per month element not found');
+      console.log("Price per month element not found");
       return;
     }
 
     // Get the billing cycle label element
-    const billingCycleLabel = card.querySelector('.billing-cycle-label');
+    const billingCycleLabel = card.querySelector(".billing-cycle-label");
     if (!billingCycleLabel) {
-      console.log('Billing cycle label element not found');
+      console.log("Billing cycle label element not found");
       return;
     }
 
     // Get the plan term details element
-    const planTermDetails = card.querySelector('.plan-term-details');
+    const planTermDetails = card.querySelector(".plan-term-details");
     if (!planTermDetails) {
-      console.log('Plan term details element not found');
+      console.log("Plan term details element not found");
       return;
     }
 
@@ -114,12 +119,12 @@ toggles.forEach((toggle, index) => {
     if (pricePerMonth.textContent.trim() === pricing[cardType].monthly) {
       // Update to annually
       pricePerMonth.textContent = pricing[cardType].annually;
-      billingCycleLabel.textContent = 'Annual Plan';
-      planTermDetails.textContent = billingDetails.annually;
+      billingCycleLabel.textContent = "Annual Plan";
+      planTermDetails.textContent = billingDetails.annually[cardType];
     } else {
       // Update to monthly
       pricePerMonth.textContent = pricing[cardType].monthly;
-      billingCycleLabel.textContent = 'Monthly Plan';
+      billingCycleLabel.textContent = "Monthly Plan";
       planTermDetails.textContent = billingDetails.monthly;
     }
   });
