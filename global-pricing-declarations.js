0f84f37f35a68f7f9278b086302cc68dc8450020
Austin Thesing <athesing@point.com>
2023-11-03 10:14:17 -0600

    Moved pricing details to a global declaration

---
+++ global-pricing-declarations.js
@@ -0,0 +1,25 @@
+// Pricing information
+const pricing = {
+  "Sole Proprietor": {
+    monthly: "199",
+    annually: "169",
+  },
+  "S Corporation": {
+    monthly: "299",
+    annually: "255",
+  },
+  "Sole Proprietor or S Corporation": {
+    monthly: "299",
+    annually: "255",
+  },
+};
+
+// Billing details
+const billingDetails = {
+  monthly: "Billed monthly • Cancel anytime",
+  annually: {
+    "Sole Proprietor": "Billed $2,028 annually",
+    "S Corporation": "Billed $3,060 annually",
+    "Sole Proprietor or S Corporation": "Billed $3,060 annually",
+  },
+};
--- pricing-swap.js
+++ pricing-swap.js
@@ -1,29 +1,3 @@
-// Pricing information
-const pricing = {
-  "Sole Proprietor": {
-    monthly: "199",
-    annually: "169",
-  },
-  "S Corporation": {
-    monthly: "299",
-    annually: "255",
-  },
-  "Sole Proprietor or S Corporation": {
-    monthly: "299",
-    annually: "255",
-  },
-};
-
-// Billing details
-const billingDetails = {
-  monthly: "Billed monthly • Cancel anytime",
-  annually: {
-    "Sole Proprietor": "Billed $2,028 annually",
-    "S Corporation": "Billed $3,060 annually",
-    "Sole Proprietor or S Corporation": "Billed $3,060 annually",
-  },
-};
-
 // Function to update pricing
 function updatePricing(card) {
   // Get the card type
